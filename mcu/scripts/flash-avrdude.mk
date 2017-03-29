bb9646abd8ac7d902e365e43fd8aeda9fb3dc462
Christian Vogelgsang <chris@vogelgsang.org>
2017-03-29 15:42:14 +0200

    cleanup avrdude rules

--- mcu/scripts/flash-avrdude.mk
+++ mcu/scripts/flash-avrdude.mk
@@ -1,43 +1,73 @@
+# flash-avrdude.mk
+# rules for flashing with avrdude
+
+# detect (single) serial port on system
 OS := $(shell uname -s)
 ifeq "$(OS)" "Darwin"
-LDR_PORT ?= $(shell ls /dev/cu.usbserial-* | tail -n 1)
+SERIAL_PORT ?= $(shell ls /dev/cu.usbserial-* | tail -n 1)
 else
-LDR_PORT ?= $(shell ls /dev/ttyUSB* | tail -n 1)
+SERIAL_PORT ?= $(shell ls /dev/ttyUSB* | tail -n 1)
 endif
 
-# commands
+# avrdude binary
 AVRDUDE ?= avrdude
-AVRDUDE_WRITE_FLASH  = -U flash:w:$1
-AVRDUDE_WRITE_FUSE   = -U lfuse:w:$(CONFIG_AVRDUDE_LFUSE):m -U hfuse:w:$(CONFIG_AVRDUDE_HFUSE):m
-AVRDUDE_LOCK_BOOTLOADER = -U lock:w:0x0F:m
 
-# combine flags
-AVRDUDE_FLAGS += -p $(CONFIG_AVRDUDE_MCU) -c $(CONFIG_AVRDUDE_PROGRAMMER)
-ifeq "$(CONFIG_AVRDUDE_HAS_PORT)" "y"
-AVRDUDE_FLAGS += -P $(LDR_PORT) -b $(CONFIG_AVRDUDE_BAUD)
+# write flash: $1 = hex file
+AVRDUDE_WRITE_FLASH  = -U flash:w:$1
+
+# set fuses
+ifneq "$(CONFIG_AVRDUDE_LFUSE)" ""
+AVRDUDE_WRITE_FUSE += -U lfuse:w:$(CONFIG_AVRDUDE_LFUSE):m
 endif
-ifdef AVRDUDE_DEBUG
+ifneq "$(CONFIG_AVRDUDE_HFUSE)" ""
+AVRDUDE_WRITE_FUSE += -U hfuse:w:$(CONFIG_AVRDUDE_HFUSE):m
+endif
+ifneq "$(CONFIG_AVRDUDE_EFUSE)" ""
+AVRDUDE_WRITE_FUSE += -U efuse:w:$(CONFIG_AVRDUDE_EFUSE):m
+endif
+
+# lock byte
+ifneq "$(CONFIG_AVRDUDE_LOCK_BYTE)" ""
+AVRDUDE_LOCK_BOOTLOADER = -U lock:w:$(CONFIG_AVRDUDE_LOCK_BYTE):m
+endif
+
+# setup base parameters of avrdude:
+# device (required)
+AVRDUDE_FLAGS += -p $(CONFIG_AVRDUDE_MCU)
+# programmer (required)
+AVRDUDE_FLAGS += -c $(CONFIG_AVRDUDE_PROGRAMMER)
+# port (optional)
+ifeq "$(CONFIG_AVRDUDE_PORT)" "auto"
+AVRDUDE_FLAGS += -P $(SERIAL_PORT)
+else
+ifneq "$(CONFIG_AVRDUDE_PORT)" ""
+AVRDUDE_FLAGS += -P $(CONFIG_AVRDUDE_PORT)
+endif
+endif
+# baud (optional)
+ifneq "$(CONFIG_AVRDUDE_BAUD)" ""
+AVRDUDE_FLAGS += -b $(CONFIG_AVRDUDE_BAUD)
+endif
+# debug (optional)
+ifeq "$(CONFIG_AVRDUDE_DEBUG)" "1"
 AVRDUDE_FLAGS += -v -v -v -v
+else
+AVRDUDE_QUIET = -q
 endif
 
 read-sig:
-	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) -U signature:r:sig.txt:h
-	@echo -n " device signature: "
-	@cat sig.txt
-	@rm -f sig.txt
+	@echo "device signature: "
+	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_QUIET) -U signature:r:-:h
 
-read-fuse:
-	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) -U lfuse:r:lfuse.txt:h -U hfuse:r:hfuse.txt:h -U efuse:r:efuse.txt:h
-	@echo -n " lfuse: "
-	@cat lfuse.txt
-	@echo -n " hfuse: "
-	@cat hfuse.txt
-	@echo -n " efuse: "
-	@cat efuse.txt
-	@rm -f lfuse.txt hfuse.txt efuse.txt
+read-fuses:
+	@echo "fuses: low, high, extra"
+	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_QUIET) -U lfuse:r:-:h -U hfuse:r:-:h -U efuse:r:-:h
 
 read-flash:
 	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) -U flash:r:flash.img:r
+
+write-fuses:
+	$(H)$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_WRITE_FUSE)
 
 # prog rule for firmware
 # $1 = firmware.hex file
