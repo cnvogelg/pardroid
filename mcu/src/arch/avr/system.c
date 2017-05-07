f338ab0dc646dd9650f919801991bb0a3fffb814
Christian Vogelgsang <C.Vogelgsang@web.de>
2017-05-07 10:32:08 +0200

    teensy20 fixes

--- mcu/src/arch/avr/system.c
+++ mcu/src/arch/avr/system.c
@@ -2,8 +2,17 @@
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
 
+#include "autoconf.h"
+
 void system_init(void)
 {
+#if (CONFIG_MACH == teensy20)
+  // set teensy clock to 16 MHz
+#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+#define CPU_16MHz       0x00
+  CPU_PRESCALE(CPU_16MHz);
+#endif
+
   // default watchdog: 500ms
   cli();
   wdt_enable(WDTO_500MS);
