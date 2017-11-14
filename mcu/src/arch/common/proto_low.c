d70ac6d441033832cae7b77f6ad6649ab6db70f6
Christian Vogelgsang <C.Vogelgsang@web.de>
2017-11-14 15:05:02 +0100

    mk20: started gpio

--- mcu/src/arch/common/proto_low.c
+++ mcu/src/arch/common/proto_low.c
@@ -1,23 +1,67 @@
 #include "autoconf.h"
 #include "types.h"
 
+#include "pario_pins.h"
 #include "proto_low.h"
+
+/* signal names:
+   IN
+      POUT -> clk
+      SELECT -> cflg
+      STROBE -> strobe
+   OUT
+      ACK -> ack
+      BUSY -> rak
+*/
+
+#define ddr(x)    pario_data_ddr(x)
+#define dout(x)   pario_set_data(x)
+#define din()     pario_get_data()
+
+#define clk()     pario_get_pout()
+#define cflg()    pario_get_select()
+#define strobe()  pario_get_strobe()
+
+#define rak_lo()  pario_busy_lo()
+#define rak_hi()  pario_busy_hi()
+#define ack_lo()  pario_ack_lo()
+#define ack_hi()  pario_ack_hi()
+
 
 void proto_low_init(u08 status)
 {
+    pario_init();
 }
 
 u08 proto_low_get_cmd(void)
 {
-  return 0;
+  // clock is low -> no command
+  if(clk()) {
+    return 0xff;
+  }
+
+  // read data (command nybble)
+  u08 cmd = din() & 0x0f;
+
+  // 5bit: cflag
+  if(cflg()) {
+    cmd |= 0x10;
+  }
+
+  return cmd;
 }
 
 void proto_low_action(void)
 {
+  rak_lo();
 }
 
 void proto_low_end(u08 status)
 {
+  u08 val = (status & 0xf0) | 0xf;
+  dout(val);
+  while(!clk()) {}
+  rak_hi();
 }
 
 void proto_low_read_word(u16 v)
@@ -50,10 +94,12 @@ u08  proto_low_read_block(u16 num_words, u08 *buffer, u16 chn_ext)
 
 void proto_low_ack_lo(void)
 {
+  ack_lo();
 }
 
 void proto_low_ack_hi(void)
 {
+  ack_hi();
 }
 
 u08  proto_low_set_status(u08 status)
@@ -63,4 +109,5 @@ u08  proto_low_set_status(u08 status)
 
 void proto_low_wait_cflg_hi(void)
 {
+  while(!cflg()) {}
 }
---
+++ mcu/src/arch/mk20/mach-teensy32/pario_pins.h
@@ -0,0 +1,134 @@
+#ifndef PARIO_PINS_H
+#define PARIO_PINS_H
+
+#include <stdint.h>
+#include "kinetis.h"
+
+/* Parallel Port Setup
+
+
+               Parallel Port    Teensy 3.2 Pin    Core Pin    Dir
+    /STROBE        1               23               PC2        in
+    DATA0          2               2                PD0
+    DATA1          3               14               PD1
+    DATA2          4               7                PD2
+    DATA3          5               8                PD3
+    DATA4          6               6                PD4
+    DATA5          7               20               PD5
+    DATA6          8               21               PD6
+    DATA7          9               5                PD7
+    /ACK          10               22               PC1        out
+    BUSY          11               17               PB1        out
+    POUT          12               16               PB0        in
+    SELECT        13               15               PC0        in
+    GND           18-22            GND
+*/
+
+/* PE/PS = pull up enable, PFE=filter enable, SRE=slew rate enable, DSE=drive strength */
+#define DATA_PORT_FLAGS  PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_SRE | PORT_PCR_DSE
+#define IN_PORT_FLAGS    PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE
+#define OUT_PORT_FLAGS   PORT_PCR_SRE | PORT_PCR_DSE
+
+#define DATA_MASK        0xff
+#define STROBE_MASK      (1<<2)
+#define ACK_MASK         (1<<1)
+#define BUSY_MASK        (1<<1)
+#define POUT_MASK        (1<<0)
+#define SELECT_MASK      (1<<0)
+
+static inline void pario_init(void)
+{
+  // ----- port mux -----
+  // -- Port B:
+  // POUT (IN)
+  PORTB_PCR0 = PORT_PCR_MUX(1) | IN_PORT_FLAGS;
+  // busy (OUT)
+  PORTB_PCR1 = PORT_PCR_MUX(1) | OUT_PORT_FLAGS;
+
+  // -- Port C:
+  // SELECT (IN)
+  PORTC_PCR0 = PORT_PCR_MUX(1) | IN_PORT_FLAGS;
+  // ack (OUT)
+  PORTC_PCR1 = PORT_PCR_MUX(1) | OUT_PORT_FLAGS;
+  // strobe (in)
+  PORTC_PCR2 = PORT_PCR_MUX(1) | IN_PORT_FLAGS;
+
+  // -- Port D: data port
+  PORTD_GPCLR = (DATA_MASK << 16) | PORT_PCR_MUX(1) | DATA_PORT_FLAGS;
+
+  // ----- DDR -----
+  // Port B: BUSY, POUT
+  uint32_t old = GPIOB_PDDR & ~(BUSY_MASK | POUT_MASK);
+  GPIOB_PDDR = old | BUSY_MASK;
+
+  // Port C: SELECT, ACK, STROBE
+  old = GPIOC_PDDR & ~(SELECT_MASK | ACK_MASK | STROBE_MASK);
+  GPIOC_PDDR = old | ACK_MASK;
+
+  // Port D: DATA
+  GPIOD_PDDR &= ~0xff; // INPUT
+
+  // ----- DATA -----
+  // Port B:
+  GPIOB_PSOR = BUSY_MASK | POUT_MASK;
+  // Port C:
+  GPIOC_PSOR = ACK_MASK | SELECT_MASK | STROBE_MASK;
+  // Port D:
+  GPIOD_PSOR = DATA_MASK;
+}
+
+static inline void pario_data_ddr(uint8_t ddr)
+{
+  GPIOD_PDDR = (GPIOD_PDDR & ~(DATA_MASK)) | ddr;
+}
+
+static inline uint8_t pario_get_data(void)
+{
+  return (uint8_t)GPIOD_PDIR;
+}
+
+static inline void pario_set_data(uint8_t data)
+{
+  GPIOD_PDOR = (GPIOD_PDOR & ~(DATA_MASK)) | data;
+}
+
+// input lines
+
+static inline int pario_get_pout(void)
+{
+  return (GPIOB_PDIR & POUT_MASK) == POUT_MASK;
+}
+
+static inline int pario_get_select(void)
+{
+  return (GPIOC_PDIR & SELECT_MASK) == SELECT_MASK;
+}
+
+static inline int pario_get_strobe(void)
+{
+  return (GPIOC_PDIR & STROBE_MASK) == STROBE_MASK;
+}
+
+// output lines
+
+static inline void pario_busy_hi(void)
+{
+  GPIOB_PSOR = BUSY_MASK;
+}
+
+static inline void pario_busy_lo(void)
+{
+  GPIOB_PCOR = BUSY_MASK;
+}
+
+static inline void pario_ack_hi(void)
+{
+  GPIOC_PSOR = ACK_MASK;
+}
+
+static inline void pario_ack_lo(void)
+{
+  GPIOC_PCOR = ACK_MASK;
+}
+
+#endif
